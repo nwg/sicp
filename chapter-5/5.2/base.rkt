57c42aa3039678cb81576dfed934256c298997f0
Nathaniel Griswold <nategriswold@gmail.com>
2019-11-02 19:34:36 -0500

    ex 5.11 part 1

--- chapter-5/5.2/base.rkt
+++ chapter-5/5.2/base.rkt
@@ -442,3 +442,55 @@
         (error "Unknown operation: ASSEMBLE"
                symbol))))
 
+
+(define fib-tree
+  (make-machine
+   '(n continue val)
+   (list (list '< <) (list '- -) (list '+ +))
+   '(
+   (assign continue (label fib-done))
+ fib-loop
+   (test (op <) (reg n) (const 2))
+   (branch (label immediate-answer))
+   ;; set up to compute Fib(n - 1)
+   (save continue)
+   (assign continue (label afterfib-n-1))
+   (save n)           ; save old value of n
+   (assign n 
+           (op -)
+           (reg n)
+           (const 1)) ; clobber n to n-1
+   (goto 
+    (label fib-loop)) ; perform recursive call
+ afterfib-n-1 ; upon return, val contains Fib(n - 1)
+   (restore n)
+   (restore continue)
+   ;; set up to compute Fib(n - 2)
+   (assign n (op -) (reg n) (const 2))
+   (save continue)
+   (assign continue (label afterfib-n-2))
+   (save val)         ; save Fib(n - 1)
+   (goto (label fib-loop))
+ afterfib-n-2 ; upon return, val contains Fib(n - 2)
+   ;(assign n 
+   ;        (reg val)) ; n now contains Fib(n - 2)
+   (restore n)      ; n now contains Fib(n - 1)
+   (restore continue)
+   (assign val        ; Fib(n - 2) + Fib(n - 1)
+           (op +) 
+           (reg val)
+           (reg n))
+   (goto              ; return to caller,
+    (reg continue))   ; answer is in val
+ immediate-answer
+   (assign val 
+           (reg n))   ; base case: Fib(n) = n
+   (goto (reg continue))
+ fib-done)))
+
+
+(set-register-contents! fib-tree 'n 7)
+(start fib-tree)
+(display "fib-tree: ")
+(display (get-register-contents fib-tree 'val))
+(newline)
