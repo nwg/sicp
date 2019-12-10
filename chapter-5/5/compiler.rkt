d209a81e51101cc7c545eddcf7bda1114e86a157
Nathaniel Griswold <nategriswold@gmail.com>
2019-12-10 06:54:02 -0600

    remove compile code

--- chapter-5/5/compiler.rkt
+++ chapter-5/5/compiler.rkt
@@ -591,21 +591,3 @@
            (statements seq2))))
 
 
-(compile
- '(define (factorial n)
-    (if (= n 1)
-        1
-        (* n (factorial (- n 1)))))
- 'val
- 'next)
-
-(compile
- '(define (factorial n)
-    (define (iter product counter)
-      (if (> counter n)
-          product
-          (iter (* counter product)
-                (+ counter 1))))
-    (iter 1 1))
- 'val
- 'next)
