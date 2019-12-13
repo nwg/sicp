b59f74b68aa545844ea810ca31d3992dc4a314d3
Nathaniel Griswold <nategriswold@gmail.com>
2019-12-13 08:39:39 -0600

    remove displaylns

--- chapter-5/5/run.rkt
+++ chapter-5/5/run.rkt
@@ -12,16 +12,13 @@
 
 (define (compile-and-go expression)
   (let-values ([(instructions labels) (assemble (statements (compile expression 'val 'return the-empty-compile-environment)) eceval)])
-    (displayln (pair? instructions))
     ;; ((eceval 'trace-register) 'val)
     ;; ((eceval 'trace-register) 'arg2)
     (set! the-global-environment
           (setup-environment))
 
-    (displayln "before set")
     (set-register-contents! 
      eceval 'val instructions)
-    (displayln "after set")
     (set-register-contents! 
      eceval 'flag true)
     (start eceval)))
